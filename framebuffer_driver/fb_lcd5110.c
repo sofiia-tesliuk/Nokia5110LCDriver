e66eec6acd7997928758ea3782de873d628405f0
LAnastasiia <lazorenko@ucu.edu.ua>
2020-01-21 15:04:25 +0200

    draft-1

--- framebuffer_driver/fb_lcd5110.c
+++ framebuffer_driver/fb_lcd5110.c
@@ -1,8 +1,15 @@
 /*
  * Framebuffer driver definition.
- * resources:
- *  https://habr.com/ru/post/213775/
- *  http://www.esys.ir/Files/Ref_Books/Linux/esys.ir_Embedded.Linux.System.Design.and.Development.pdf.
+ * main resource:
+ *  http://www.esys.ir/Files/Ref_Books/Linux/esys.ir_Embedded.Linux.System.Design.and.Development.pdf
+
+=   Fill up driver operations structure struct fb_ops.
+   Fill up frame buffer fixed info struct fb_fix_screeninfo.
+-   Fill up driver information structure struct fb_info.
+-   Initialize hardware registers and video memory area.
+-   Allocate and initialize color map struct fb_cmap, if necessary.7
+-   Register the fb_info structure with driver framework using register_framebuffer.
+
  *  */
 
 #include <fb.h>
@@ -12,11 +19,28 @@
 
 int fb_lcd5110_major = 29;
 
+static struct fb_info fb_info;
 
-static void __exit fb_lcd5110_exit(void);
+static struct fb_ops fb_lcd5110_ops = {
+        .owner		= THIS_MODULE,
+        .fb_read = fb_sys_read
+//        .fb_open	= ...,
+//        .fb_write	= ...,
+//        .fb_release	= ...,
+//        .fb_pan_display	= ...,
+//        .fb_fillrect	= ...,
+//        .fb_copyarea	= ...,
+//        .fb_imageblit	= ...,
+//        .fb_cursor	= ...,
+};
+
+
 static int __init  fb_lcd5110_init(void);
+static int __init fb_lcd5110_probe(struct platform_device *device);
+static void __exit fb_lcd5110_exit(void);
 
 
+/* Config for GPU. */
 static struct fb_var_screeninfo fb_lcd5110_var __init_data = {
         .xres           = 320,
         .yres           = 240,
@@ -24,37 +48,28 @@ static struct fb_var_screeninfo fb_lcd5110_var __init_data = {
         .yres_virtual   = 240,
         .width          = 84,
         .height         = 48,
+        .left_margin    = 0,
+        .right_margin   = 0,
+        .upper_margin   = 0,
+        .lower_margin   = 0,
         .bits_per_pixel = 1,
         .red            = { 0, 1, 0 },
         .green          = { 0, 1, 0 },
         .blue           = { 0, 1, 0 },
         .activate       = FB_ACTIVATE_NOW,
-        .vmode          = FB_VMODE_NONINTERLACED,
+        .vmode          = FB_VMODE_NONINTERLACED
 };
 
 
+/* Config for device. */
 static struct fb_fix_screeninfo fb_lcd5110_fix __init_data = {
         .id          = "LCD5110",
         .type        = FB_TYPE_PACKED_PIXELS,
         .visual      = FB_VISUAL_MONO10,  // white = 1, black = 0
         .accel       = FB_ACCEL_NONE,
-        .line_length = fb_lcd5110_var.xres * fb_lcd5110_var.bits_in_pixel / 8,
+        .line_length = fb_lcd5110_var.xres * fb_lcd5110_var.bits_in_pixel / 8
 };
 
-
-static struct fb_ops fb_lcd5110_ops = {
-        .fb_read      = fb_sys_read,
-        // TODO fb_write
-        // TODO fb_copyarea
-};
-
-
-struct lcd5110 {
-    struct device *device;
-    struct fb_info *info;
-    unsigned int num_pages;
-    struct lcd5110_page *pages;
-};
 
 struct lcd5110_page {
     unsigned short x;
@@ -65,28 +80,11 @@ struct lcd5110_page {
 };
 
 
-static void ssd1963_update(struct fb_info *info, struct list_head *pagelist)
-{
-    struct ssd1963 *item = (struct ssd1963 *)info->par;
-    struct page *page;
-    int i;
-
-    list_for_each_entry(page, pagelist, lru) {
-        item->pages[page->index].must_update=1;
-    }
-
-    //Copy changed pages.
-    for (i=0; i<item->pages_count; i++) {
-        if (item->pages[i].must_update) {
-            item->pages[i].must_update=0;
-            ssd1963_copy(item, i);
-        }
-    }
-}
-
-static struct fb_deferred_io lcd5110_defio = {
-        .delay          = HZ / 20,
-        .deferred_io    = &lcd5110_update,
+struct lcd5110 {
+    struct device *device;
+    struct fb_info *info;
+    unsigned int num_pages;
+    struct lcd5110_page *pages;
 };
 
 
@@ -101,28 +99,11 @@ static int __init  fb_lcd5110_init(void) {
 }
 
 
-static int __exit fb_lcd5110_remove(struct platform_device *device) {
-    /* Free internal structures. */
-    struct fb_info *info = platform_get_drvdata(device);
-    struct ssd1963 *item = (struct lcd5110 *)info->par;
-    if (info) {
-        unregister_framebuffer(info);
-        ssd1963_pages_free(item);
-        ssd1963_video_free(item);
-        framebuffer_release(info);
-        kfree(item);
-    }
-    return 0;
-}
-
-
 static void __exit fb_lcd5110_exit(void) {
     struct fb_info *info = platform_get_drvdata(device);
-    struct ssd1963 *item = (struct ssd1963 *)info->par;
+    struct lcd5110 *item = (struct lcd5110 *)info->par;
     if (info != nullptr) {
         unregister_framebuffer(info);
-        ssd1963_pages_free(item);
-        ssd1963_video_free(item);
         framebuffer_release(info);
         kfree(item);
     }
@@ -131,9 +112,25 @@ static void __exit fb_lcd5110_exit(void) {
 }
 
 
+static int fb_lcd5110_probe(struct probe* devp) {
+    struct device *device = &devp->dev;
+    struct fb_info *info;  // The main driver structure.
+    struct fb_private_field *prv;  // Private field - contains hardware state of the graphics card.
+
+    if ((info = framebuffer_alloc(sizeof(struct fb_private_field), &devp->dev)) && info != NULL) {
+        if (register_framebuffer(info) > 0) {
+            return 0;
+        } else
+            return -EINVAL;  // Failed to register our framebuffer driver in linux device tree.
+
+    } else
+        return -ENOMEM;  // Alloc failed due to lack of memory.
+}
+
+
 struct platform_driver lcd5110_driver = {
-        .probe = lcd5110_probe, // ??
-        .remove = lcd5110_remove,  // done
+        .probe  = fb_lcd5110_probe,
+        .remove = fb_lcd5110_exit,
         .driver = { .name = "lcd5110" }
 };
 
@@ -141,4 +138,4 @@ struct platform_driver lcd5110_driver = {
 module_init(fb_lcd5110_init);
 module_exit(fb_lcd5110_exit);
 
-MODULE_LICENSE("GPL v2");
+MODULE_LICENSE("GPL");
