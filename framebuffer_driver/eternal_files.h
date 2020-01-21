6903f310e5dd3abf3bf40af2aa0c5f3f7936f84d
LAnastasiia <lazorenko@ucu.edu.ua>
2020-01-21 18:17:14 +0200

    v-0.1 + try-fix: copy-paste Linux's sys_copyarea function

---
+++ framebuffer_driver/eternal_files.h
@@ -0,0 +1,7 @@
+
+#ifndef NOKIA5110LCDRIVER_ETERNAL_FILES_H
+#define NOKIA5110LCDRIVER_ETERNAL_FILES_H
+
+void sys_copyarea(struct fb_info *p, const struct fb_copyarea *area);
+
+#endif //NOKIA5110LCDRIVER_ETERNAL_FILES_H
